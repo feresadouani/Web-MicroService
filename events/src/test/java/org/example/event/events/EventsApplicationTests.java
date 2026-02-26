6fbfde46dee6e410f10b717213c1f71bbff8e426
hsounaSellami <hsouna.sellami1@gmail.com>
2026-02-26 01:59:49 +0100

    Add event +config eureka and apigateway

---
+++ events/src/test/java/org/example/event/events/EventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.event.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
